c2520d8227c73320d7c95979d907fff83b42535c
IceSofticon30th <ice.softicon30th@gmail.com>
2015-03-24 02:08:22 +0900

    名前が返る

--- js/towns.js
+++ js/towns.js
@@ -59,8 +59,39 @@ function getGou() {
     }
 }
 
+function getPollingPlace(id) {
+    if (id == 0) {
+        return null;
+    }
+    /*
+    if (loaded == false) {
+        return {};
+    }
+    */
+    var obj = pollingPlaceList['http://linkdata.org/resource/rdf1s3099i#' + id];
+    var poll = {
+        id: obj['http://linkdata.org/property/rdf1s3099i#%E6%8A%95%E7%A5%A8%E5%8C%BA'][0].value,
+        name: obj['http://linkdata.org/property/rdf1s3099i#%E6%8A%95%E7%A5%A8%E6%89%80'][0].value,
+        place: obj['http://linkdata.org/property/rdf1s3099i#%E6%8A%95%E7%A5%A8%E6%89%80%E4%BD%8F%E6%89%80'][0].value,
+        lat: obj['http://www.w3.org/2003/01/geo/wgs84_pos#lat'][0].value,
+        long: obj['http://www.w3.org/2003/01/geo/wgs84_pos#long'][0].value,
+        match: obj['http://linkdata.org/property/rdf1s3099i#%E8%A9%B2%E5%BD%93%E4%BD%8F%E6%89%80'][0].value
+    }
+    return poll;
+}
+
 setTowns();
 document.getElementById('SearchButton').onclick = function () {
     var id = judgeKwkm(getTownName(), getChou(), getBan(), getGou());
-    window.alert(id);
+    window.alert(getPollingPlace(id).name);
+    console.log(getPollingPlace(id));
 };
+
+/*
+var pollingPlaceList;
+var loaded = true;
+$.getJSON('../data/tsuzukiku_senkyoku_rdf.json', function(data) {
+    pollingPlaceList = data;
+    loaded = true;
+});
+*/
