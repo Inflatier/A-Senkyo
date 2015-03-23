382d3810a02c5fe70ad9fb2e176dee44ad417a7f
IceSofticon30th <ice.softicon30th@gmail.com>
2015-03-24 00:26:31 +0900

    入力されたデータを取得する関数を追加

---
+++ js/towns.js
@@ -0,0 +1,28 @@
+function setTowns() {
+    var t = document.getElementById('selectTown');
+    var towns = getTowns();
+    for (var i = 0; i < towns.length; i++) {
+        var opt = document.createElement('option');
+        opt.innerHTML = towns[i];
+        t.appendChild(opt);
+    }
+    return;
+}
+
+function getSelectedTownName() {
+    var town = document.getElementById('selectTown');
+    var index = town.selectedIndex;
+    return town.options.item(index).value;
+}
+
+function getBan() {
+    var ban = document.getElementById('ban');
+    return parseInt(ban.value);
+}
+
+function getGou() {
+    var gou = document.getElementById('gou');
+    return parseInt(gou.value);
+}
+
+setTowns();
